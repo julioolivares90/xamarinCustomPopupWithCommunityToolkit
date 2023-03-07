f022789cc3a63ed2d201bc71f475a130127aa68b
Julio Olivares <julioolivares90@hotmail.com>
2023-03-06 20:22:32 -0600

    second commit

---
+++ CustomPopup/CustomPopup/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace CustomPopup
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //para mostrar resultado usar Navigation.ShowPopupAsync
+         var result = Navigation.ShowPopupAsync(new MyPopUp() {  IsLightDismissEnabled = false }); //IsLightDismissEnabled evita que se cierre el popup al hacer tap fuera del popup
+        }
+    }
+}
---
+++ CustomPopup/CustomPopup/MyPopUp.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomPopup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPopUp : Popup
+    {
+        public MyPopUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Dismiss(null);
+
+            //para retornar algo a la otra page se utiliza Dismiss(aqui objeto que se quiere devolver)
+        }
+    }
+}
